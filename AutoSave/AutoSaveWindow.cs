e9c7f5055ada4646bdcd15899a75222b3640a425
PolsCommits <paul.rauca@outlook.com>
2020-03-26 23:21:31 +0000

    Add AutoSave script; Update README.md;

---
+++ AutoSave/AutoSaveWindow.cs
@@ -0,0 +1,69 @@
+﻿// Available under Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0) - https://creativecommons.org/licenses/by-sa/3.0/
+// Adapted from https://wiki.unity3d.com/index.php/AutoSave
+using UnityEngine;
+using UnityEditor;
+using System;
+using UnityEditor.SceneManagement;
+
+class AutoSaveWindow : EditorWindow 
+{
+    private bool autoSaveScene = true;
+	private bool showMessage = true;
+	private bool isStarted = false;
+	private int intervalScene;	
+	private DateTime lastSaveTimeScene = DateTime.Now;
+
+    [MenuItem ("Window/Auto Save")]
+
+    public static void  ShowWindow () 
+	{
+        EditorWindow.GetWindow(typeof(AutoSaveWindow));
+    }
+    
+    void OnGUI () 
+    {
+		GUILayout.Label ("Auto Save Options:", EditorStyles.boldLabel);
+		autoSaveScene = EditorGUILayout.BeginToggleGroup ("Auto save", autoSaveScene);
+		intervalScene = EditorGUILayout.IntSlider ("Interval (minutes)", intervalScene, 1, 10);
+		
+        if(isStarted) 
+        {
+			EditorGUILayout.LabelField ("Last save:", "" + lastSaveTimeScene);
+		}
+
+		EditorGUILayout.EndToggleGroup();
+		showMessage = EditorGUILayout.BeginToggleGroup ("Show Message", showMessage);
+		EditorGUILayout.EndToggleGroup ();
+    }
+
+    void Update()
+    {
+		if(autoSaveScene) 
+        {
+			if(DateTime.Now.Minute >= (lastSaveTimeScene.Minute+intervalScene) || DateTime.Now.Minute == 59 && DateTime.Now.Second == 59)
+            {
+				saveScene();
+			}
+		} 
+        else 
+        {
+			isStarted = false;
+		}
+ 
+	}
+
+    void saveScene() 
+    {
+		EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene());
+		lastSaveTimeScene = DateTime.Now;
+		isStarted = true;
+		
+        AutoSaveWindow repaintSaveWindow = (AutoSaveWindow)EditorWindow.GetWindow (typeof (AutoSaveWindow));
+		repaintSaveWindow.Repaint();
+        
+        if(showMessage)
+        {
+            Debug.Log("Saved scene " + EditorSceneManager.GetActiveScene().name);
+        }
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # unity-utilities
 Collection of useful and time-saving Unity scripts
+
+## Auto Save
+
+Adapted script from https://wiki.unity3d.com/index.php/AutoSave to work in the latest versions of Unity (Tested on Unity 2018.4.2f1 & 2019.3.2f1)
